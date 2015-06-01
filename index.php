7511ee84e6f53015c8ba058a299e4350e4ab20ae
João Cezar <cezarg16@gmail.com>
2015-06-01 00:22:05 -0300

    nada alterado

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 	<title>Site Teste</title>
 </head>
 <body>
-<<<<<<< HEAD
 	<section id="pagina">
 		<header id="cabecalho">
 			<nav>
@@ -23,16 +22,5 @@
 		
 		<footer id="rodape"></footer>
 	</section>
-=======
-	<nav>
-		<ul>
-			<li><a href="javascript:void[0]">Página Inicial</a></li>
-			<li><a href="javascript:void[0]">Sobre</a></li>
-			<li><a href="javascript:void[0]">Contatos</a></li>
-			<li><a href="javascript:void[0]">Blog</a></li>
-			<li><a href="javascript:void[0]">Patrocinadores</a></li>
-		</ul>
-	</nav>
->>>>>>> c3df402f85f2db672253287e47d91bdcdc5e7c53
 </body>
 </html>
